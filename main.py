923932c6e4f85b5b5c66eee7ba3ed772828fd58e
aarontbk <aarontebeka@gmail.com>
2021-07-04 02:03:05 +0300

    third try, closer but still doesnt count folders in folders and it counts folders as files

--- main.py
+++ main.py
@@ -6,27 +6,33 @@
 
 import os.path
 
+Folders_to_verify = []
+file_count = 0
 path1 = input("Give a path: ")
 path2 = input("Give another path: ")
 
 def count_files(path):
-    file_count: int = 0
+    global file_count, Folders_to_verify
     files = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
     for i in range(len(files)):
-        if os.path.isfile(path + r"\"" + files[i]):
-            file_count += 1
-            print(file_count)
+        if os.path.isdir(path + r"\"" + files[i]):
+            Folders_to_verify.append(path + r"\"" + files[i])
         else:
-            count_files(os.listdir(path + r"\"" + files[i]))
+            file_count += 1
+
+    for k in range(len(Folders_to_verify)):
+        count_files(Folders_to_verify[k])
+
     return file_count
 
 
 file_count1 = count_files(path1)
+file_count = 0
 file_count2 = count_files(path2)
 
 if file_count1 > file_count2:
     print(path1 + "   --->  has more files")
 elif file_count1 == file_count2:
-    print("both paths have: " + file_count1 + " files")
+    print("both paths have: " + str(file_count1) + " files")
 else:
     print(path2 + "   --->  has more files")
