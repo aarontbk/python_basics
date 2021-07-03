134bb1c29b3651c37839c2ccb97ec56534645b15
aarontbk <aarontebeka@gmail.com>
2021-07-03 22:33:49 +0300

    test commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi2, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi3, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 # you forgot to blablabla
 # Press the green button in the gutter to run the script.
