192d2de22b2ef63f297dd3cce00b7c6db0cb87ff
aarontbk <86892502+aarontbk@users.noreply.github.com>
2021-07-03 22:30:48 +0300

    Update main.py
    cr from beni

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi2, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+# you forgot to blablabla
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
