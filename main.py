99931f398333488940ed422be961a6465bf52b22
aarontbk <aarontebeka@gmail.com>
2021-07-04 00:53:05 +0300

    First try, not working

--- main.py
+++ main.py
@@ -8,10 +8,23 @@
 
 import os.path
 
+file_count1 = 0
+file_count2 = 0
+
 path1 = input("Give a path: ")
 path2 = input("Give another path: ")
-file_count1 = sum(len(files) for _, _, files in os.walk(path1))
-file_count2 = sum(len(files) for _, _, files in os.walk(path2))
+
+def count_files(file_count, path):
+    dir = os.listdir(path)
+    for i in dir (len(dir)):
+        if os.path.isfile(path + r"\"" + i):
+            file_count += 1
+        else:
+            count_files(file_count, os.listdir(path + r"\"" + i), dir)
+    return file_count
+
+first = count_files(file_count1, path1)
+second = count_files(file_count2, path2)
 
 if file_count1 > file_count2:
     print(path1 + "   --->  has more files")
