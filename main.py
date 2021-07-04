d929577cb7a09e6564b2dc2aced6d90f5293aad4
aarontbk <aarontebeka@gmail.com>
2021-07-04 11:32:10 +0300

    Fourth try, it works!!!

--- main.py
+++ main.py
@@ -6,33 +6,30 @@
 
 import os.path
 
-Folders_to_verify = []
-file_count = 0
 path1 = input("Give a path: ")
 path2 = input("Give another path: ")
 
 def count_files(path):
-    global file_count, Folders_to_verify
-    files = [os.path.splitext(filename)[0] for filename in os.listdir(path)]
+    global file_count
+    file_count = 0
+    files = os.listdir(path)
     for i in range(len(files)):
-        if os.path.isdir(path + r"\"" + files[i]):
-            Folders_to_verify.append(path + r"\"" + files[i])
+        if os.path.isdir(path + "\\" + files[i]):
+            file_count += count_files(path + "\\" + files[i])
         else:
             file_count += 1
-
-    for k in range(len(Folders_to_verify)):
-        count_files(Folders_to_verify[k])
 
     return file_count
 
 
 file_count1 = count_files(path1)
-file_count = 0
 file_count2 = count_files(path2)
+print("path1 has: " + str(file_count1) + " files")
+print("path2 has: " + str(file_count2) + " files")
 
 if file_count1 > file_count2:
-    print(path1 + "   --->  has more files")
+    print("path1 has more files")
 elif file_count1 == file_count2:
-    print("both paths have: " + str(file_count1) + " files")
+    print("both paths have the same amount of files")
 else:
-    print(path2 + "   --->  has more files")
+    print("path2 has more files")
