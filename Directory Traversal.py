8a1063c67a49e78b1287f704e76cdf5cf487e01a
Benitbk <benitbk@gmail.com>
2021-07-05 18:34:47 +0300

    CR

--- Directory Traversal.py
+++ Directory Traversal.py
@@ -3,16 +3,19 @@
 # notes: you can use os.listdir and os.path.isdir but not os.walk
 # hint: to implement os.walk read about DFS and recursion
 
+# CR: after fixing a Cr remove it, unless you are not sure if it's fixed
 # CR: create a class named DirectoryTraversal or something like that organize the code
 # DirectoryTraversal(path: string)
 # count_files() -> int
 import os.path
 
+# CR: move this class to a separate module (named after the class). You can read about python modules and files
 class DirectoryTraversal:
-    path: str = ""
-    def __init__(self, path) -> None:
+    path: str = "" # CR: type is optional for inline variables (I like it but just so you know it's less important)
+    def __init__(self, path) -> None: # CR: path should be typed
         self.path = path    #instance variable
-
+        
+        
     def count_files(self) -> int:
         file_count: int = 0
         files = os.listdir(self.path)
@@ -24,7 +27,8 @@ class DirectoryTraversal:
                 file_count += 1
 
         return file_count
-
+    
+    # CR: Move all of this (below) code to a function (e.g. compare_two_paths_file_count) in a static Main class 
 path1: str = input("Give a path: ")
 path2: str = input("Give another path: ")
 
@@ -35,9 +39,12 @@ count2: int = d2.count_files()
 print("path1 has: " + str(count1) + " files")
 print("path2 has: " + str(count2) + " files")
 
+# CR: merge the three prints with a single one: f"Path {path1} has {count1} files and path {path2} has {count2} files"
 if count1 > count2:
     print("path1 has more files")
 elif count1 == count2:
     print("both paths have the same amount of files")
 else:
     print("path2 has more files")
+
+    # CR: the next next will be to print the files instead of counting them. After that I want you to try to think how to implement it without recursion (a little hard).
