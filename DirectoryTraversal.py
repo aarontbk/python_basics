8214caaea5af7266d719c8d67b916a843cf1d5cc
aarontbk <aarontebeka@gmail.com>
2021-07-06 18:21:22 +0300

    fixed CR

--- DirectoryTraversal.py
+++ DirectoryTraversal.py
@@ -4,18 +4,22 @@ import os.path
 class DirectoryTraversal:
     path: str = ""
 
+
     def __init__(self, path: str) -> None:
         self.path = path
 
-
     def count_files(self) -> int:
+        files: list = os.listdir(self.path)
         file_count: int = 0
-        files = os.listdir(self.path)
-        for i in range(len(files)):
-            path_in_path: str = os.path.join(self.path, files[i])
-            if os.path.isdir(path_in_path):
-                file_count += DirectoryTraversal(path_in_path).count_files()
+        for file in files:
+            file_path: str = os.path.join(self.path, file)
+
+            if os.path.isdir(file_path):
+                file_count += DirectoryTraversal(file_path).count_files()
             else:
                 file_count += 1
-
+                print(self.path + "\\" + file + " , size: " + str(os.path.getsize(file_path)))
         return file_count
+
+
+
--- Main.py
+++ Main.py
@@ -1,21 +1,21 @@
 from DirectoryTraversal import *
 
-def compare_two_paths_file_count():
-    path1: str = input("Give a path: ")
-    path2: str = input("Give another path: ")
 
-    d1: object = DirectoryTraversal(path1)
-    d2: object = DirectoryTraversal(path2)
+class Main:
+    @staticmethod
+    def compare_two_paths_file_count():
+        path1: str = input("Give a path: ")
+        path2: str = input("Give another path: ")
 
-    count1: int = d1.count_files()
-    count2: int = d2.count_files()
+        d1 = DirectoryTraversal(path1)
+        d2 = DirectoryTraversal(path2)
 
-    print("path " + str(path1) + " has " + str(count1) + " files and path " + str(path2) + " has " + str(
-        count2) + " files")
+        count1: int = d1.count_files()
+        count2: int = d2.count_files()
 
+        print(f"path {path1} has {count1} files and path {path2} has {count2} files")
 
-# Main
+Main.compare_two_paths_file_count()
 
-compare_two_paths_file_count()
-
-# CR: the next next will be to print the files instead of counting them. After that I want you to try to think how to implement it without recursion (a little hard).
+# CR: the next next will be to print the file names and size instead of counting them. After that I want you to try to think how to implement it without recursion (a little hard).
+# list, queue, stack
