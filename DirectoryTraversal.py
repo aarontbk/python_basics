30f59a0de0a614ccb4919b59cdb32a536b65e314
aarontbk <aarontebeka@gmail.com>
2021-07-08 12:43:25 +0300

    implemented function "print_files_using_queue" in class DirectoryTraversal.py

--- DirectoryTraversal.py
+++ DirectoryTraversal.py
@@ -1,9 +1,11 @@
 import os.path
+import queue
+from queue import Queue
+from typing import Any
 
 
 class DirectoryTraversal:
     path: str = ""
-
 
     def __init__(self, path: str) -> None:
         self.path = path
@@ -21,5 +23,15 @@ class DirectoryTraversal:
                 print(self.path + "\\" + file + " , size: " + str(os.path.getsize(file_path)))
         return file_count
 
+    def print_files_using_queue(self) -> None:
+        to_visit: Queue = queue.Queue()
+        to_visit.put(self.path)
 
+        while not to_visit.empty():
+            current: str = to_visit.get()
+            print(current)
+            file_path: str = os.path.join(self.path, current)
 
+            if os.path.isdir(file_path):
+                for child in os.listdir(file_path):
+                    to_visit.put(child)
--- Main.py
+++ Main.py
@@ -5,17 +5,19 @@ class Main:
     @staticmethod
     def compare_two_paths_file_count():
         path1: str = input("Give a path: ")
-        path2: str = input("Give another path: ")
-
         d1 = DirectoryTraversal(path1)
+        d1.print_files_using_queue()
+
+        path2: str = input("Give another path: ")
         d2 = DirectoryTraversal(path2)
+        d2.print_files_using_queue()
 
-        count1: int = d1.count_files()
-        count2: int = d2.count_files()
+        # count1: int = d1.count_files()
+        # count2: int = d2.count_files()
+        #
+        # print(f"path {path1} has {count1} files and path {path2} has {count2} files")
 
-        print(f"path {path1} has {count1} files and path {path2} has {count2} files")
+
+# Main
 
 Main.compare_two_paths_file_count()
-
-# CR: the next next will be to print the file names and size instead of counting them. After that I want you to try to think how to implement it without recursion (a little hard).
-# list, queue, stack
